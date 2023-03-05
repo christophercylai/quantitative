c022d03bc740607c16793d6317abba6e1c6765a6
christophercylai <christophercylai@outlook.com>
2023-03-05 11:15:14 -0500

    autotools ready to merge

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-
 set -e
 
-if [[ ! -d "../.venv" ]]; then
-    pushd ..
+rootdir=$(pwd)
+
+pushd ..
+mkdir -p quant
+cd quant
+quantdir=$(pwd)
+if [[ ! -d ".venv" ]]; then
     python -m venv .venv
-    source .venv/bin/activate
-    pip install pybind11==2.10.1
-    popd
 fi
+source .venv/bin/activate
+pip install pybind11==2.10.1
+popd
 
 export PY_FLAGS=$(python3 -m pybind11 --includes)
 export PY_EXT=$(python3-config --extension-suffix)
@@ -18,3 +22,8 @@ mkdir -p build
 cd build
 ../configure
 make
+
+cp src/.libs/libquant.so.0.0.0 ${quantdir}
+cp ${rootdir}/quant.sh ${quantdir}
+cd ${quantdir}
+ln -s libquant.so.0.0.0 quant$(python3-config --extension-suffix)
